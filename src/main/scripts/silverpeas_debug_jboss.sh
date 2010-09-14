6f3aa2340d3581b4977c608504329ce0f59fa758
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-09-14 20:03:20 +0000

    Correction for quotes in linux
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/services/installation/trunk@2344 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- src/main/scripts/silverpeas_debug_jboss.sh
+++ src/main/scripts/silverpeas_debug_jboss.sh
@@ -2,7 +2,7 @@
 # ------ silverpeas_start_jboss.sh -----------
 cd $JBOSS_HOME/bin
 
-export JAVA_OPTS=-server -Xms512m -Xmx512m -XX:MaxPermSize=128m -Dorg.jboss.logging.Log4jService.catchSystemOut=false -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=5000,suspend=n,server=y
+export JAVA_OPTS="-server -Xms512m -Xmx512m -XX:MaxPermSize=128m -Dorg.jboss.logging.Log4jService.catchSystemOut=false -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=5000,suspend=n,server=y"
 
 # Add Silverpeas Properties root repository to path
 JBOSS_CLASSPATH=$SILVERPEAS_HOME/properties
