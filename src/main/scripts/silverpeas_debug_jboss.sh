67b36fd55da010b61e730571c9f7427e67e0fa39
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2010-09-28 14:58:25 +0000

    Removing wrong instanciation of the Classpath
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/services/installation/trunk@2435 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- src/main/scripts/silverpeas_debug_jboss.sh
+++ src/main/scripts/silverpeas_debug_jboss.sh
@@ -5,7 +5,7 @@ cd $JBOSS_HOME/bin
 export JAVA_OPTS="-server -Xms512m -Xmx512m -XX:MaxPermSize=128m -Dorg.jboss.logging.Log4jService.catchSystemOut=false -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=5000,suspend=n,server=y"
 
 # Add Silverpeas Properties root repository to path
-JBOSS_CLASSPATH=$SILVERPEAS_HOME/properties
-export JBOSS_CLASSPATH
+# JBOSS_CLASSPATH=$SILVERPEAS_HOME/properties
+# export JBOSS_CLASSPATH
 
 sh run.sh &
