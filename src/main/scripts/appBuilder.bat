9322e51d2592f53f18f73d02b4e20af72a7786d3
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-02-11 16:25:33 +0100

    improving #assembly #config :  Fix a mispelling in appBuilder.bat

--- src/main/scripts/appBuilder.bat
+++ src/main/scripts/appBuilder.bat
@@ -2,5 +2,5 @@
 
 set CLASSPATH=%SILVERPEAS_HOME%\bin\jar\*
 
-java  -classpath "%CLASSPATH% -Dsilverpeas.home="%SILVERPEAS_HOME%" org.silverpeas.applicationbuilder.ApplicationBuilder -r ext_repository %1
+java  -classpath "%CLASSPATH%" -Dsilverpeas.home="%SILVERPEAS_HOME%" org.silverpeas.applicationbuilder.ApplicationBuilder -r ext_repository %1
 pause
