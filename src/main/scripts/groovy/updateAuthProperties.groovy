5f4f39455ce9d9a1e91fe9bc86cb53a749e492ff
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2013-02-13 18:00:21 +0100

    Update also the properties of the SQL domain authentication located in the old place com/stratelia/silverpeas/authentication

--- src/main/scripts/groovy/updateAuthProperties.groovy
+++ src/main/scripts/groovy/updateAuthProperties.groovy
@@ -1,19 +1,26 @@
 #!/usr/bin/env groovy
 
-final String AuthPropertiesPath = "${SILVERPEAS_HOME}/properties/org/silverpeas/authentication/"
-final String TempSuffix         = '.tmp'
-final String TemplateToReplace  = 'com.stratelia.'
+final String OldAuthPropertiesPath = "${SILVERPEAS_HOME}/properties/com/stratelia/silverpeas/authentication/"
+final String AuthPropertiesPath    = "${SILVERPEAS_HOME}/properties/org/silverpeas/authentication/"
 
-new File(AuthPropertiesPath).eachFile { properties ->
-  if (properties.isFile()) {
-    FileWriter updatedProperties = new FileWriter(properties.path + TempSuffix)
-    new FileReader(properties).transformLine(updatedProperties) { line ->
-      if (line.contains(TemplateToReplace))
-        line = line.replaceAll(TemplateToReplace, 'org.')
-      if (!line.contains('SQLPasswordEncryption'))
-        line
+def updateAllAuthPropertiesIn(String directory) {
+  final String TempSuffix            = '.tmp'
+  final String TemplateToReplace     = 'com.stratelia.'
+  new File(directory).eachFile { properties ->
+    if (properties.isFile()) {
+      FileWriter updatedProperties = new FileWriter(properties.path + TempSuffix)
+      new FileReader(properties).transformLine(updatedProperties) { line ->
+        if (line.contains(TemplateToReplace))
+          line = line.replaceAll(TemplateToReplace, 'org.')
+        if (!line.contains('SQLPasswordEncryption'))
+          line
+      }
+      properties.delete()
+      new File(properties.path + TempSuffix).renameTo(properties)
     }
-    properties.delete()
-    new File(properties.path + TempSuffix).renameTo(properties)
-  }
+  } 
 }
+
+updateAllAuthPropertiesIn OldAuthPropertiesPath
+updateAllAuthPropertiesIn AuthPropertiesPath
+
