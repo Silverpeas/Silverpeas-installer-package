b60950a403dcb225b2e3ab77a9943033f2256dba
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2014-02-28 18:49:13 +0100

    Remove old unusable scripts

--- src/main/scripts/removeDuplicateContents.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set CLASSPATH=%SILVERPEAS_HOME%\bin\jar\*
-
-java -classpath "%CLASSPATH%" -Ddbbuilder.home="%SILVERPEAS_HOME%" org.silverpeas.migration.contentmanagement.DuplicateContentRemovingApplication
-
-echo.
-
-pause
